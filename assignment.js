0abab03c882a37b12979e7f08526412189b82b90
Tasmia Khaled <khaled.tasmia33@gmail.com>
2021-01-22 14:01:12 +0600

    final submission

--- assignment.js
+++ assignment.js
@@ -15,46 +15,46 @@ function budgetCalculator(watch, mobile, laptop) {
         let total = (watch * 50) + (mobile * 100) + (laptop * 500);
         return total;
     }
-    else{
+    else {
         return -1;
     }
 }
 
 //hotelCost
-function hotelCost(days){
-    if(days>=1 && days<=10){
-        let cost = days*100;
+function hotelCost(days) {
+    if (days >= 1 && days <= 10) {
+        let cost = days * 100;
         return cost;
     }
-    else if(days>=11 && days<=20){
+    else if (days >= 11 && days <= 20) {
         let remainingdays = days - 10;
-        let cost = (10*100) + (remainingdays*80);
+        let cost = (10 * 100) + (remainingdays * 80);
         return cost;
     }
-    else if(days>=21){
+    else if (days >= 21) {
         let remainingdays = days - 20;
-        let cost = (10*100) + (10*80) + (remainingdays*50);
+        let cost = (10 * 100) + (10 * 80) + (remainingdays * 50);
         return cost;
     }
-    else if(days<1){
+    else if (days < 1) {
         return -1;
     }
 }
 
 //megaFriend
-function megaFriend(arr){
-    if(arr.length == 0){
+function megaFriend(arr) {
+    if (arr.length == 0) {
         return -1;
     }
-    else if(arr.length>=1){
+    else if (arr.length >= 1) {
         let elementLength = arr[0].length;
-    let element = arr[0];
-    for(var i=1; i<arr.length; i++){
-        if(arr[i].length>elementLength){
-            elementLength = arr[i].length;
-             element = arr[i];
+        let element = arr[0];
+        for (var i = 1; i < arr.length; i++) {
+            if (arr[i].length > elementLength) {
+                elementLength = arr[i].length;
+                element = arr[i];
+            }
         }
-    }
-    return element;
+        return element;
     }
 }
